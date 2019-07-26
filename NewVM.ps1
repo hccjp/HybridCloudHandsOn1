5e45cc01b2e78fbbe6484b4c95d82d0c5844a550
Masahiko Ebisuda <ebibibi@gmail.com>
2019-07-26 21:50:15 +0900

    new file.

---
+++ NewVM.ps1
@@ -0,0 +1,127 @@
+# Select location
+$location = Read-Host "location?"
+# Create variables to store the location and resource group names.
+$location = "tokyo"
+$ResourceGroupName = "WS2016byPS"
+
+New-AzResourceGroup `
+  -Name $ResourceGroupName `
+  -Location $location
+
+# Create variables to store the storage account name and the storage account SKU information
+$StorageAccountName = "mystorageaccount"
+$SkuName = "Standard_LRS"
+
+# Create a new storage account
+$StorageAccount = New-AzStorageAccount `
+  -Location $location `
+  -ResourceGroupName $ResourceGroupName `
+  -Type $SkuName `
+  -Name $StorageAccountName
+
+Set-AzCurrentStorageAccount `
+  -StorageAccountName $storageAccountName `
+  -ResourceGroupName $resourceGroupName
+
+# Create a subnet configuration
+$subnetConfig = New-AzVirtualNetworkSubnetConfig `
+  -Name mySubnet `
+  -AddressPrefix 192.168.1.0/24
+
+# Create a virtual network
+$vnet = New-AzVirtualNetwork `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -Name MyVnet `
+  -AddressPrefix 192.168.0.0/16 `
+  -Subnet $subnetConfig
+
+# Create a public IP address and specify a DNS name
+$pip = New-AzPublicIpAddress `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -AllocationMethod Static `
+  -IdleTimeoutInMinutes 4 `
+  -Name "mypublicdns$(Get-Random)"
+
+# Create an inbound network security group rule for port 3389
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig `
+  -Name myNetworkSecurityGroupRuleRDP `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 1000 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 3389 `
+  -Access Allow
+
+# Create an inbound network security group rule for port 80
+$nsgRuleWeb = New-AzNetworkSecurityRuleConfig `
+  -Name myNetworkSecurityGroupRuleWWW `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 1001 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access Allow
+
+# Create a network security group
+$nsg = New-AzNetworkSecurityGroup `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -Name myNetworkSecurityGroup `
+  -SecurityRules $nsgRuleRDP,$nsgRuleWeb
+
+# Create a virtual network card and associate it with public IP address and NSG
+$nic = New-AzNetworkInterface `
+  -Name myNic `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -SubnetId $vnet.Subnets[0].Id `
+  -PublicIpAddressId $pip.Id `
+  -NetworkSecurityGroupId $nsg.Id
+
+# Define a credential object to store the username and password for the VM
+$UserName='demouser'
+$Password='Password@123'| ConvertTo-SecureString -Force -AsPlainText
+$Credential=New-Object PSCredential($UserName,$Password)
+
+# Create the VM configuration object
+$VmName = "VirtualMachinelatest"
+$VmSize = "Standard_A1"
+$VirtualMachine = New-AzVMConfig `
+  -VMName $VmName `
+  -VMSize $VmSize
+
+$VirtualMachine = Set-AzVMOperatingSystem `
+  -VM $VirtualMachine `
+  -Windows `
+  -ComputerName "MainComputer" `
+  -Credential $Credential
+
+$VirtualMachine = Set-AzVMSourceImage `
+  -VM $VirtualMachine `
+  -PublisherName "MicrosoftWindowsServer" `
+  -Offer "WindowsServer" `
+  -Skus "2016-Datacenter" `
+  -Version "latest"
+
+# Sets the operating system disk properties on a VM.
+$VirtualMachine = Set-AzVMOSDisk `
+  -VM $VirtualMachine `
+  -CreateOption FromImage | `
+  Set-AzVMBootDiagnostics -ResourceGroupName $ResourceGroupName `
+  -StorageAccountName $StorageAccountName -Enable |`
+  Add-AzVMNetworkInterface -Id $nic.Id
+
+
+# Create the VM.
+New-AzVM `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $location `
+  -VM $VirtualMachine
+
+
